54264f538b905dc41263b6cd43fd3f968129e64d
AlexNesvit <alex.nesvit2000@gmail.com>
2024-12-13 10:56:58 +0100

    React Basics 02 - Pokédex

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyButton from '/src/components/MyButton/MyButton.jsx'
+import PokemonCard from '/src/components/PokemonCard/PokemonCard.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,15 +11,10 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <PokemonCard />
       </div>
       <MyButton />
-      <h1>Hello World</h1>
+      <h1>React Basics 02 - Pokédex</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count + is {count}
@@ -33,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      
     </>
   )
 }
