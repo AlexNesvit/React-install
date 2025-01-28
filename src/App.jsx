4a2e11ca73b630a0d643579fba9f19bb75bca2a1
AlexNesvit <alex.nesvit2000@gmail.com>
2025-01-28 11:57:59 +0100

    props 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import MyButton from '/src/components/MyButton/MyButton.jsx'
 import PokemonCard from '/src/components/PokemonCard/PokemonCard.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const pokemonList = [
+      { name: "pikachu", imgSrc: "pikachu.png" },
+      { name: "mew", imgSrc: "mew.png" },
+  ];
 
   return (
     <>
       <div>
-        <PokemonCard />
+          <PokemonCard pokemon={pokemonList[0]} />
       </div>
       <MyButton />
-      <h1>React Basics 02 - Pokédex</h1>
+      <h1>React Basics 04 - Pokédex - Passer des props</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count + is {count}
--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,27 +1,10 @@
-function PokemonCard() {
-    // On récupère le premier Pokémon de la liste
-    const pokemon = pokemonList[1];
-
+const PokemonCard = ({ pokemon }) => {
     return (
-        <figure>
-            {
-                pokemon.imgSrc 
-                ? <img src={pokemon.imgSrc} alt={pokemon.name} />
-                : <p>???</p>
-            }
-            <figcaption>{pokemon.name}</figcaption>
-        </figure>
+        <div>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.imgSrc} alt={pokemon.name} />
+        </div>
     );
-}
-
-const pokemonList = [
-    {
-        name: "bulbasaur",
-        imgSrc: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-        name: "mew",
-    },
-];
+};
 
 export default PokemonCard;
