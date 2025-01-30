2f470d714f090669ed728ae7d4e805834203ed8a
AlexNesvit <alex.nesvit2000@gmail.com>
2025-01-30 13:30:51 +0100

    React Basics 05 - Pokédex - Typer les props avec TypeScript

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -1,10 +1,21 @@
-const PokemonCard = ({ pokemon }) => {
-    return (
-        <div>
-            <h1>{pokemon.name}</h1>
-            <img src={pokemon.imgSrc} alt={pokemon.name} />
-        </div>
-    );
+import React from "react";
+
+interface PokemonProps {
+  pokemon: {
+    name: string;
+    imgSrc?: string;
+  };
+}
+
+const PokemonCard: React.FC<PokemonProps> = ({ pokemon }) => {
+  return (
+    <div className="p-4 border rounded-lg shadow-md bg-white">
+      <h2 className="text-lg font-bold">{pokemon.name}</h2>
+      {pokemon.imgSrc && (
+        <img src={pokemon.imgSrc} alt={pokemon.name} className="w-32 h-32 mx-auto mt-2" />
+      )}
+    </div>
+  );
 };
 
 export default PokemonCard;
